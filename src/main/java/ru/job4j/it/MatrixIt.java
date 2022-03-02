087bc8ad3c66936d198f2bb425f84476040a9dbf
User <iuheniomail@yandex.ru>
2022-03-02 17:42:53 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539 #270621]

---
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,43 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Реализуйте методы next и hasNext.
+ * нужно вывести элементы двумерного массива
+ */
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    /**
+     * в цикле while следующая проверка:
+     * - ряд существует (пока наш ряд меньше общего колич.рядов), и
+     * - столбец существует (если колич. столбцов == столбцу к которому мы подошли в данный момент,
+     *   то значит в этом ряду столбцов больше нет и column опять становится 0 (column = 0)
+     *   а мы переходим к следующему ряду (row++), и начинаем выводить столбцы теперь этого ряда, начиная с column = 0.
+     * @return определяет, существуют ли еще ряды, результа которых мы не вывели.
+     */
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && data[row].length == column) {
+            column = 0;
+            row++;
+        }
+        return row < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+}
