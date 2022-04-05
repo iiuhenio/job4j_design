8c7808d606887c65c5cf45171097819d475e713a
User <iuheniomail@yandex.ru>
2022-04-05 09:27:23 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,6 +14,15 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
 
+    public static void main(String[] args) {
+        Calendar date = new GregorianCalendar(1985, Calendar.JANUARY, 16);
+        User user1 = new User("Anton", 2, date);
+        User user2 = new User("Anton", 2, date);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
