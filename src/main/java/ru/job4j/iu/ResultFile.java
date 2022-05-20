27bc9b335a747c74a4f797c865909a9978b0ff63
User <iuheniomail@yandex.ru>
2022-05-20 17:17:08 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/iu/ResultFile.java
@@ -0,0 +1,41 @@
+package ru.job4j.iu;
+
+import java.io.FileOutputStream;
+
+/**
+ * В первом блоке курса было задание на вывод таблицы умножения на консоль.
+ * Вам нужно записать результат вычисления в файл.
+ *
+ *  Напишем программу, которая записывает текст в файл
+ *  Класс java.io.FileOutputStream позволяет записать данные в файл
+ */
+public class ResultFile {
+    public static void main(String[] args) {
+
+        /**
+         * конструктор класса FileOutputStream принимает имя файла.
+         * Файл будет создан в корне проекта
+         * Любой ресурс должен быть закрыт, по этому используется конструкция try-with-resources
+         */
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+
+            /**
+             * для записи используется метод out.write
+             * этот метод принимает массив байт.
+             * по этому строку преобразовали в массив байтов
+             */
+            int[][] array = new int[10][10];
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = (i + 1) * (j + 1);
+                    int x = array[i][j];
+                    String y = Integer.toString(x);
+                    out.write(y.getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
